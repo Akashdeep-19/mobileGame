fcd144f6900df23fa2d14a464273e380e06bf9c2
Akashdeep-S9 <akasdeeps19@gmail.com>
2019-04-11 18:01:17 +0530

    added touch feature

--- game.js
+++ game.js
@@ -2,7 +2,18 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
 }
-
+var r = 10;
 function draw (){
   background(50);
+  fill(200,200,100);
+  ellipse(width/2,height/2,r,r);
+}
+
+function touchStarted(){
+  if(r < 100){
+    r += 10;
+  }
+  else {
+    r -= 60;
+  }
 }
