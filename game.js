28194089b5569ea43430efdfe50433bfb9b246db
Akashdeep-S9 <39194549+Akashdeep-S9@users.noreply.github.com>
2019-04-13 17:55:14 +0530

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ var balls = [];
 function setup(){
   
 
-  const canvasElt = createCanvas(400, 800).elt;
+  const canvasElt = createCanvas(400, 700).elt;
   canvasElt.style.width = '100%', canvasElt.style.height = '100%';
   rot = createVector(0,1);
 }
